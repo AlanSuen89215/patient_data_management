52f2703f1f8b1219279585ed5ccdb3db4bf95a29
Alan <csuen2@my.centennialcollege.ca>
2022-11-15 10:35:30 -0400

    Update end point to use cloud

--- EndPointConfig.js
+++ EndPointConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-    urlLogin: 'http://10.0.2.2:5000/login',
-    urlListAllPatients: 'http://10.0.2.2:5000/patients',
-    urlListAllPatientsInCritical: 'http://10.0.2.2:5000/critical-patients',
-    urlAddPatient: 'http://10.0.2.2:5000/patients',
-    urlGetPatientById: 'http://10.0.2.2:5000/patients/:id',
-    urlGetClinicalDataById: 'http://10.0.2.2:5000/patients/:id/tests',
-    urlAddTreatmentRecord: 'http://10.0.2.2:5000/patients/:id/treatments',
-    urlViewTreatmentRecordByPatientId: 'http://10.0.2.2:5000/patients/:id/treatments'
+    urlLogin: 'https://alansuen89215-mapd713-project.herokuapp.com/login',
+    urlListAllPatients: 'https://alansuen89215-mapd713-project.herokuapp.com/patients',
+    urlListAllPatientsInCritical: 'https://alansuen89215-mapd713-project.herokuapp.com/critical-patients',
+    urlAddPatient: 'https://alansuen89215-mapd713-project.herokuapp.com/patients',
+    urlGetPatientById: 'https://alansuen89215-mapd713-project.herokuapp.com/patients/:id',
+    urlGetClinicalDataById: 'https://alansuen89215-mapd713-project.herokuapp.com/patients/:id/tests',
+    urlAddTreatmentRecord: 'https://alansuen89215-mapd713-project.herokuapp.com/patients/:id/treatments',
+    urlViewTreatmentRecordByPatientId: 'https://alansuen89215-mapd713-project.herokuapp.com/patients/:id/treatments'
 }
